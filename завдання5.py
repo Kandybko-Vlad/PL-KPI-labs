de3b766f7f6faf8de9a6c56f16ad31c7319a26a3
Kandybko-Vlad <vlad.5t5t5t@gmail.com>
2025-09-09 10:20:16 +0300

    Add files via upload

---
+++ завдання5.py
@@ -0,0 +1,6 @@
+a = int(input("Введіть тризначне число, яке хочете обернути: "))
+b = a % 10 # the last number
+c = a // 100 # the first number
+d = (a // 10) % 10 # the middle number
+x = b * 100 + d * 10 + c
+print("Обернуте число:", x)
