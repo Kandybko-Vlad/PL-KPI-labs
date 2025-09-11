05d9083ba3aa668b7c065a6cf374a1a5c72e1059
Kandybko Vladyslav <vlad.5t5t5t@gmail.com>
2025-09-11 14:00:00 +0300

    3commit

--- lesson2/завдання_7.py
+++ lesson2/завдання_7.py
@@ -1,6 +1,7 @@
 a = int(input("Скільки буде чисел в списку? "))
 l = []
 for i in range(a):
-    b = float(input("Введіть число номер/s"/i))
+    b = float(input(f"Введіть число номер {i}"))
     if b % 5 == 0:
         l.append(b)
+print(l)
